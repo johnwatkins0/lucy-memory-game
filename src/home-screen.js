387c75d6d5b1a3c48ed9e9ff711c99dc10f6a2f4
johnwatkins0 <johnwatkins0@gmail.com>
2018-11-04 22:13:43 -0600

    Remove unused file

--- src/home-screen.js
+++ src/home-screen.js
@@ -2,21 +2,18 @@ import React from 'react';
 
 import { PREGAME, ROBOT_SHOULD_PLAY, FAILED } from './state/status';
 
-const HomeScreen = ({ status, setStatus }) => {
-    return (
-        [PREGAME, FAILED].indexOf(status) > -1 && (
-            <div className="failure-overlay">
-                <h1>{FAILED === status ? 'Game Over' : 'Lucy Memory Game'}</h1>
-                <button
-                    onMouseDown={() => {
-                        setStatus(ROBOT_SHOULD_PLAY);
-                    }}
-                >
-                    {FAILED === status ? 'Try again?' : 'Start'}
-                </button>
-            </div>
-        )
+const HomeScreen = ({ status, setStatus }) =>
+    [PREGAME, FAILED].indexOf(status) > -1 && (
+        <div className="failure-overlay">
+            <h1>{FAILED === status ? 'Game Over' : 'Lucy Memory Game'}</h1>
+            <button
+                onMouseDown={() => {
+                    setStatus(ROBOT_SHOULD_PLAY);
+                }}
+            >
+                {FAILED === status ? 'Try again?' : 'Start'}
+            </button>
+        </div>
     );
-};
 
 export default HomeScreen;
